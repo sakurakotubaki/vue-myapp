17386757887b1255a62600fbf554e205baae6820
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2022-04-17 21:39:12 +0900

    テキストを修正

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_FIREBASE_APIKEY,
   authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
@@ -13,6 +9,5 @@ const firebaseConfig = {
   appId: process.env.VUE_APP_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const config = initializeApp(firebaseConfig);
 export default config;
