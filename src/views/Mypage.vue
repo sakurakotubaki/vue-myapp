775929648f7c9d5d4ea89eff77ffc8a7235ab73a
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2022-04-25 23:57:48 +0900

    ユーザーの退会機能を作りたい?

--- src/views/Mypage.vue
+++ src/views/Mypage.vue
@@ -2,15 +2,28 @@
   <div>
     <h1>ユーザー管理</h1>
     <button @click="logOut">ログアウト</button>
+    <button @click="deleteAccount">退会する</button>
   </div>
 </template>
 
 <script>
-import { signOut } from "firebase/auth";
+import { signOut, deleteUser } from "firebase/auth";
 import { auth } from "@/config/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import store from "@/store";
 import { mapActions, mapGetters } from 'vuex';
 
 export default {
+  mounted: function () {
+    onAuthStateChanged(auth, (user) => {
+      store.commit("setIsLogin", user !== null)
+    });
+  },
+  computed: {
+    isLoginState() {
+      return this.$store.state.isLogin;
+    }
+  },
   methods: {
     async logOut() {
       try {
@@ -20,6 +33,16 @@ export default {
       } catch (error) {
         console.error(error)
         console.log('ログアウトに失敗しました')
+      }
+    },
+    async deleteAccount() {
+      try {
+        await deleteUser(auth.user)
+        await this.$router.push("/");
+        console.log('アカウントを削除しました')
+      } catch (error) {
+        console.error(error)
+        console.log('アカウントの削除に失敗しました')
       }
     }
   }
