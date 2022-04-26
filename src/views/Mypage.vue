002558e5777df332c0383235acd2a66c42034731
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2022-04-26 23:46:47 +0900

    watchを使うべきなのか?

--- src/views/Mypage.vue
+++ src/views/Mypage.vue
@@ -3,17 +3,24 @@
     <h1>ユーザー管理</h1>
     <button @click="logOut">ログアウト</button>
     <button @click="deleteAccount">退会する</button>
+    <p>{{ userAuth }}</p>
   </div>
 </template>
 
 <script>
-import { signOut, deleteUser } from "firebase/auth";
-import { auth, user } from "@/config/firebase";
+import { signOut, deleteUser, reauthenticateWithCredential } from "firebase/auth";
+import { auth, user, credential } from "@/config/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import store from "@/store";
 import { mapActions, mapGetters } from 'vuex';
 
 export default {
+  computed: {
+    async userAuth() {
+      await reauthenticateWithCredential(user, credential)
+      console.log(this.user)
+    }
+  },
   methods: {
     async logOut() {
       try {
@@ -28,7 +35,7 @@ export default {
     async deleteAccount() {
       if(confirm("ユーザーを削除しますか？")) {
         try {
-          await deleteUser(user.uid)
+          await deleteUser(user)
           await this.$router.push("/");
           console.log('アカウントを削除しました')
         } catch (error) {
