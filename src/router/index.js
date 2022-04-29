fe3a66f2de797352906d0ab343a5924bac9c6230
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2022-04-29 13:46:53 +0900

    バグが発生中!

--- src/router/index.js
+++ src/router/index.js
@@ -43,6 +43,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/Mypage.vue')
   },
   {
+    path: '/update_email',
+    name: 'updateEmail',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/UpdateEmail.vue')
+  },
+  {
     path: "*",
     // redirect: "/",
     name: "NotFound",
