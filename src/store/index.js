462882df6912c1d913c1f8192c8a6e35d57b3cc0
abeshi03 <k_913411@yahoo.co.jp>
2022-05-02 23:28:38 +0900

    userをグローバルstateで定義

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import { auth } from '@/config/firebase'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    user: {},
+    user: null,
     isLogin: false
   },
   getters: {
     user: state => state.user
   },
   mutations: {
-    SET_USER: state => { state.user = auth.currentUser },
+    setUser: (state, setUser) => { state.user = setUser },
     setIsLogin: (state, changeLoginState) => { state.isLogin = changeLoginState }
-  },
-  actions: {
-    setUser: context => { context.commit('SET_USER') }
-  },
-  modules: {
   }
 })
