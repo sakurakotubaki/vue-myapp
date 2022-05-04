009c058734f12cba6c7153a661f93e8fc5574405
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2022-05-05 07:51:20 +0900

    🔨 投稿ページへのpathを追加

---
+++ src/views/Tweet.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h1>投稿ページ</h1>
+    <button @click="logOut">ログアウト</button>
+    <div class="form-wrapper">
+      <h1>メッセージを投稿する</h1>
+      <form @submit.prevent="tweetMessage">
+        <div class="form-item">
+          <label for="tweet">投稿する内容を入力</label><br>
+          <textarea
+          name="tweet"
+          id="tweet"
+          cols="30"
+          rows="10"
+          v-model="tweetValue"
+          ></textarea>
+        </div>
+        <p v-if="!formValidation" v-bind:style="{ color: 'red' }">140文字未満で入力してください。</p>
+        <Button text="投稿" color="#6a88eb" type="submit" />
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { signOut } from 'firebase/auth'
+import { auth } from '@/config/firebase'
+import Button from '@/components/Button.vue'
+
+export default {
+  components: {
+    Button
+  },
+  name: 'MyPage',
+  data () {
+    return {
+      tweetValue: ''
+    }
+  },
+  computed: {
+    user () {
+      return this.$store.state.user
+    },
+    formValidation () {
+      return this.tweetValue.length < 140
+    }
+  },
+  methods: {
+    async logOut () {
+      try {
+        await signOut(auth)
+        await this.$router.push('/')
+        console.log('ログアウトしました')
+      } catch (error) {
+        console.error(error)
+        console.log('ログアウトに失敗しました')
+      }
+    },
+    tweetMessage () {
+      console.log(this.tweetValue)
+    }
+  }
+}
+</script>
